0ba2709fdbd92f6392bee2ca42ca65e8e267f66d
AndresdoSantos <andres.dosantosbritoamaral@gmail.com>
2023-04-11 21:24:44 -0300

    feat: rocket class on YouTube

--- App.tsx
+++ App.tsx
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, TextInput, Button, Text } from 'react-native'
+import { useEffect, useState } from 'react'
+import { useMMKV, useMMKVObject } from 'react-native-mmkv'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { styles } from './styles'
+
+// const storage = new MMKV({ id: 'myapp' })
+
+type User = {
+  name: string
+  email: string
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  const storage = useMMKV({ id: 'myapp' })
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+
+  const [user, setUser] = useMMKVObject<User>('user')
+
+  const fetchUser = () => {
+    const data = storage.getString('user')
+
+    setUser(data ? JSON.parse(data) : {})
+  }
+
+  const handleSave = () => {
+    setUser({
+      email,
+      name,
+    })
+  }
+
+  useEffect(() => {
+    const listener = storage.addOnValueChangedListener((changedKey) => {
+      const newValue = storage.getString(changedKey)
+
+      console.log('NOVO VALOR - ', newValue)
+
+      fetchUser()
+    })
+
+    return () => listener.remove()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        placeholder="Nome..."
+        style={styles.input}
+        onChangeText={setName}
+        value={name}
+      />
+      <TextInput
+        placeholder="Email..."
+        style={styles.input}
+        onChangeText={setEmail}
+        value={email}
+      />
+
+      <Button title="Salvar" onPress={handleSave} />
+
+      <Text>{user?.name}</Text>
+      <Text>{user?.email}</Text>
+    </View>
+  )
+}
---
+++ styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    paddingHorizontal: 32,
+    gap: 16,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#333',
+    borderRadius: 5,
+    height: 56,
+    padding: 16,
+  },
+})
